2ab08bb9f08e047a4ba4ee02c202fb4b380445c3
Lincoln Mullen <lincoln@lincolnmullen.com>
2022-12-22 14:20:30 -0500

    Define school model and migrations

--- dissertations/models.py
+++ dissertations/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    aha_school_id = models.BigIntegerField(
+        unique=True,
+        verbose_name="AHA school ID",
+        help_text="The identifier used by the AHA for a school",
+    )
+    name = models.CharField(help_text="The name of the school", max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
