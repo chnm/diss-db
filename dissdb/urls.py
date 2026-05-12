f6a325e11fc73b9da885fabc03fb19da271262f4
sscott710 <sscott2@eagles.bridgewater.edu>
2026-05-12 13:59:35 -0400

    beta committee networks

--- dissdb/urls.py
+++ dissdb/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
     path("contributing/", views.contributing, name="contributing"),
+    path("network_viz/", views.network_viz, name="network_viz"),
     path(
         "dissertations/",
         views.FilteredDissertationListView.as_view(),
@@ -40,6 +41,7 @@ urlpatterns = [
     path('scholars/api/<int:pk>/', views.ScholarDetailAPI.as_view()),
     path('api/get_viz_data/<int:pk>', views.get_viz_data, name="get_viz_data"),
     path('api/get_viz_data_complex/<int:pk>', views.get_viz_data_complex, name="get_viz_data_complex"),
+    path("api/school-network/<int:school_id>/", views.get_school_network_data, name="school-network-data"),
     # path("scholarsapi/<int:pk>/", views.ScholarDetail.as_view()),
     # path('', include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
